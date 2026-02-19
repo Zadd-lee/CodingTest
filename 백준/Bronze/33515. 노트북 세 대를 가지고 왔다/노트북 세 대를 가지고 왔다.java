597a3792c7bbe880195fe8d6e13d78828d74b328
김민경 <kimminkyeong0418@gmail.com>
2026-02-19 14:01:43 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        System.out.println(Math.min(i, j));
+
+    }
+}
