23fd79fc3c9870599a81005b449b362243e86f33
김민경 <kimminkyeong0418@gmail.com>
2026-03-02 19:53:55 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.09 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuffer sb = new StringBuffer();
+        
+        for(char c: myString.toCharArray()){
+            int cInt = (int)c;
+            
+            if(cInt<=90){
+                cInt+=32;
+            }
+            sb.append((char)cInt);
+        }
+        
+        return sb.toString();
+    }
+}
