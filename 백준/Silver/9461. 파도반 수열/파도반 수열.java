ad185daac438a7fc0bad71055b9ed50aa0aa56ed
김민경 <kimminkyeong0418@gmail.com>
2026-03-17 19:32:53 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        for (int j = 6; j < 101; j++) {
+            dp[j] = dp[j - 1] + dp[j - 5];
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(dp[n]).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
