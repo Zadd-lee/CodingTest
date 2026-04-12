ca417eba09aa4132f2539326f8d1ab6f7920288d
김민경 <kimminkyeong0418@gmail.com>
2026-04-12 16:22:02 +0900

    [Bronze III] Title: 핑크빈 레이드, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/31712. 핑크빈 레이드/핑크빈 레이드.java
+++ 백준/Bronze/31712. 핑크빈 레이드/핑크빈 레이드.java
@@ -2,48 +2,43 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.Map;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] periods = new int[3];
-        int[] damages = new int[3];
 
+        int[] c = new int[3];
+        int[] d = new int[3];
 
         for (int i = 0; i < 3; i++) {
-            periods[i] = sc.nextInt();
-            damages[i] = sc.nextInt();
+            c[i] = sc.nextInt();
+            d[i] = sc.nextInt();
         }
+
         int health = sc.nextInt();
 
-        for (int damage : damages) {
-            health -= damage;
+        for (int i : d) {
+            health -= i;
         }
+
         if (health <= 0) {
             System.out.println(0);
             return;
         }
 
         int sec = 1;
-
-        while (health > 0) {
+        while (true) {
             for (int i = 0; i < 3; i++) {
-                if (sec % periods[i] == 0) {
-                    health -= damages[i];
-                    if (health <= 0) {
-                        System.out.println(sec);
-                        return;
-                    }
+                if (sec % c[i] == 0) {
+                    health -= d[i];
+                }
+                if (health <= 0) {
+                    System.out.println(sec);
+                    return;
                 }
             }
             sec++;
         }
-
-
-
-
-
 
 
     }
