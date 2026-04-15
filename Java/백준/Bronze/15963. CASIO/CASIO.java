6d3f0ba80dd7c0c44d9b95b9d7cfb2baaf2451d7
김민경 <kimminkyeong0418@gmail.com>
2026-04-15 13:06:21 +0900

    [Bronze V] Title: CASIO, Time: 164 ms, Memory: 17660 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+
+
+        System.out.println(st.nextToken().equals(st.nextToken()) ? 1 : 0);
+
+    }
+}
