41ffbb941f186899b8bc0ca7f9a54f7285ef4e7b
김민경 <kimminkyeong0418@gmail.com>
2026-05-16 13:20:02 +0900

    [level 2] Title: 피로도, Time: 1.68 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    int answer ;
+    boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0,k,dungeons);
+        return answer;
+    }
+    void dfs(int depth,int k, int[][] d){
+        for(int i =0; i<d.length;i++){
+            if(!visited[i] && d[i][0]<=k){
+                visited[i] = true;
+                dfs(depth+1,k-d[i][1],d);
+                visited[i] =false;
+            }
+        }
+        answer = Math.max(answer,depth);
+    }
+}
