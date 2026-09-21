b2dad8e691e680a26667ee6be64ca5d01fffdc09
김민경 <kimminkyeong0418@gmail.com>
2026-09-21 13:08:08 +0900

    [level 2] Title: 카펫, Time: 3.08 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 61.8 MB, 시간: 0.04 ms
+메모리: 83.2 MB, 시간: 3.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 16일 12:51:05
+2026년 09월 21일 13:08:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        for(int i= 3;i<brown+yellow;i++){
-            int width = (brown+yellow)/i;
-            if(width>=i){
-                if((i-2)*(width-2)==yellow){
-                    answer[0] = width;
-                    answer[1] = i;
-                    break;
-                }
-            }
+        
+        int kan = brown+yellow;
+        for(int w = kan/2;w>1;w--){
+            if(kan%w!=0) continue;
+            int h = kan/w;
+            if((w-2)*(h-2)==yellow) return new int[]{w,h};
         }
+        
+        int[] answer = {};
         return answer;
     }
 }
