9ed99ee02cfa4b2d52e5b9d7b8fd37cb410a6d9f
김민경 <kimminkyeong0418@gmail.com>
2026-09-21 11:43:25 +0900

    [level 3] Title: 네트워크, Time: 1.39 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,31 @@
-import java.util.*;
 import java.lang.*;
+import java.util.*;
+
 class Solution {
+    boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
-        for(int start = 0;start<n;start++){
-            
-            if(visited[start]) continue;
-            
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(start);
-            while(!q.isEmpty()){
-                int node = q.poll();
-                visited[node] = true;
-                for(int i = 0;i<n;i++){
-                    if(computers[node][i]==1){
-                        if(!visited[i]){
-                            q.offer(i);
-                        }
-                    }
-                }
-            }
-            
+        visited = new boolean[n];
+        for(int i = 0;i<n;i++){
+            if(visited[i]) continue;
+            bfs(computers,i);
             answer++;
         }
         return answer;
+    }
+    
+    void bfs(int[][] maps,int start){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        while(!q.isEmpty()){
+            int now = q.poll();
+            visited[now] = true;
+            for(int i = 0;i<maps.length;i++){
+                if(maps[now][i] == 1 && !visited[i]){
+                    q.offer(i);
+                }
+            }
+        }
     }
 }
